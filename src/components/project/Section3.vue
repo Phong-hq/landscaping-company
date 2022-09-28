126945ad41821507f0398859dc4c4bdb4ae08101
Huynh Quoc Phong <phonghq@dtsmart.vn>
2022-09-28 11:24:03 +0700

    update

---
+++ src/components/project/Section3.vue
@@ -0,0 +1,91 @@
+<template>
+   <section class="project-section">
+        <div class="container">
+            <div class="pt-[120px] flex flex-col items-center section-title">
+                <p class="text-[30px] font-extrabold text-primary-hover text-center ">{{ item.title }}</p>
+            </div>
+            <div class="w-full flex-center">
+                <div class="class text-center text-primary-hover mb-6 w-[80%]" v-html="item.text"></div>
+            </div>
+            <div class="flex justify-between project-detail">
+                <div class="pr-6 mb-4 project-detail-item my-order" :style="'--order: ' + order + ';'" v-html=item.description></div>
+                <img class="project-detail-item order-2" :src="baseUrl + item.thumnail.data.attributes.url" alt="" />
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+    import { ref, onMounted, onUnmounted, computed } from 'vue';
+
+    const props =  defineProps({
+        item: {
+            type: Object,
+            required: true,
+        },
+    });
+
+    onMounted(() => {
+        console.log('asgbvdlsl');
+    })
+
+    console.log(props.item,'asdasd');
+
+    const baseUrl = ref('https://estate-strapi.srv01.dtsmart.dev')
+    // const order = ref(1)
+
+    const order = computed(() => {
+        let result = 1
+        if(props.item.type == 'right'){
+            result = 3
+        }
+        // else if(props.type == 3){
+        //     result = 4
+        // }
+        return result
+    })
+
+
+
+</script>
+
+<style lang="scss">
+    .project-section{
+        .section-title{
+            position: relative;
+            margin-bottom: 50px;
+            p::after{
+                content: "";
+                width: 60px;
+                height: 3px;
+                background-color: var(--primary-hover);
+                position: absolute;
+                bottom: 0;
+                left: 50%;
+                transform: translateX(-50%);
+                margin-bottom: -8px;
+            }
+        }
+        .project-detail-item{
+            // background-color: #878784;
+            width: 48%;
+            &.my-order{
+                order: var(--order);
+            }
+        }
+        @media (max-width: 767px) {
+            .project-detail{
+                flex-direction: column;
+                .project-detail-item{
+                    width: 100%;
+                }
+            }
+
+        }
+        &:last-child{
+            padding-bottom: 100px;
+        }
+
+    }
+</style>
